2638ddc9c1258b5a95a4eddf9ce1069bddccf41b
dain Lee <dainlee@dainui-MacBookPro.local>
2020-03-08 17:37:50 +0900

    h2 기동시 자동 데이터 삽입

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO Posts(title, author, content, created_date, modified_date) VALUES
+('코로나 바이러스 현황', '질본', '모두 잘될 거에요', CURDATE(), CURDATE()),
+('재택근무 공지', 'HR', '화이팅', '2020-03-01T015:30:16+09:00', '2020-03-08T06:30:16+09:00');
